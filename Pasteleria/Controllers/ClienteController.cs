3f620401a4c1aaeb44eb28fc166dc6fd91fc638d
Evelyn-20 <evelynmasisg05@gmail.com>
2025-10-09 22:43:39 -0600

    Vistas de Cliente

---
+++ Pasteleria/Controllers/ClienteController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pasteleria.Abstracciones.ModeloUI;
+
+namespace Pasteleria.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente/ListadoClientes
+        public IActionResult ListadoClientes()
+        {
+            // Por ahora retorna una lista vacía, luego conectarás con tu lógica de negocio
+            var clientes = new List<Cliente>();
+            return View(clientes);
+        }
+
+        // GET: Cliente/CrearCliente
+        public IActionResult CrearCliente()
+        {
+            return View();
+        }
+
+        // GET: Cliente/EditarCliente/5
+        public IActionResult EditarCliente(int id)
+        {
+            // Aquí buscarás el cliente por id
+            var cliente = new Cliente { IdCliente = id };
+            return View(cliente);
+        }
+
+        // POST: Cliente/EliminarCliente
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EliminarCliente(int id)
+        {
+            // Aquí implementarás la lógica de eliminación
+            // Por ahora solo redirige de vuelta al listado
+            return RedirectToAction(nameof(ListadoClientes));
+        }
+    }
+}
